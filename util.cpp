1c5b712557880f5278e296e1609b9bd863206e58
Carlos Duarte <carlosduarte.1@hotmail.com>
2023-12-16 21:15:11 -0700

    refactor: inline functions

--- util.cpp
+++ util.cpp
@@ -1,14 +1,3 @@
 #include "util.h"
 
-#include <QRandomGenerator>
-#include <limits>
 
-bool Util::randomBool()
-{
-    return QRandomGenerator::global()->bounded(std::numeric_limits<int>::max()) % 2;
-}
-
-int Util::randomItem(const std::vector<int> &collection)
-{
-    return collection[QRandomGenerator::global()->bounded(collection.size())];
-}
--- util.h
+++ util.h
@@ -2,11 +2,20 @@
 #define UTIL_H
 
 #include <vector>
+#include <limits>
+#include <QRandomGenerator>
 
 namespace Util
 {
-bool randomBool();
-int randomItem(const std::vector<int> &collection);
+inline bool randomBool()
+{
+    return QRandomGenerator::global()->bounded(std::numeric_limits<int>::max()) % 2;
 }
+
+inline int randomItem(const std::vector<int> &collection)
+{
+    return collection[QRandomGenerator::global()->bounded(collection.size())];
+}
+} // end namespace Util
 
 #endif // UTIL_H
